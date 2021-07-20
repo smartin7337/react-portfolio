3929c55bac6c94155e7a818a7605cf6c50cfd12d
Vossie <sv7337@outlook.com>
2021-07-19 22:14:27 -0400

    fixing deployment issues

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,7 +4,7 @@ function Portfolio() {
   return (
     <div className="portfolio-image">
       <h5>Work Examples</h5>
-      <img src="images/portfolio.jpg" alt="projects" />
+      <img src="react-portfolio/images/portfolio.jpg" alt="projects" />
     </div>
   );
 }
